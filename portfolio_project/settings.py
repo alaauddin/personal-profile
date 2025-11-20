c80e38f8da4259b0f53335961add48256dcbfb0f
alaauddin <alauddinfaya@gmail.com>
2025-11-20 11:36:50 +0300

    config: specify allowed host and configure static file serving paths.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-etbjh_m4*^e=ar-*w&zc0*c4&af5l^mrfc46q09ibc2&fngb=)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["alauddin.peak-byte.com"]
 
 
 # Application definition
@@ -115,8 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/amgausnw/alauddin.peak-byte.com/static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Media files (Images, etc.)
 MEDIA_URL = '/media/'
